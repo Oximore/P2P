830d197391e3f6c5317787b46f5da4907054dddd
blux <blux@blux-engine.(none)>
2012-02-26 17:12:42 +0100

    FichierConfiguration ok

--- paires/FichierConfiguration.java
+++ paires/FichierConfiguration.java
@@ -6,6 +6,7 @@
  */
 
 //package noeud;
+
 import java.io.RandomAccessFile;
 import java.io.IOException;
 import java.lang.String;
@@ -16,8 +17,8 @@ class FichierConfiguration{
     private int _nb_connexion;
     private long _taille;
     private int _tmp_refresh;
-    
-    FichierConfiguration(String nom){
+
+    FichierConfiguration(String fileName){
 	try{
 	    
 	    String mot = new String("Truc : ");
@@ -27,18 +28,68 @@ class FichierConfiguration{
 	    _port = recupererValeure(file,"Port : ");
 	    _nb_connexion = recupererValeure(file,"Nb Connexion : ");
 	    _tmp_refresh  = recupererValeure(file,"Tmp Refresh : ");
-	    _taille = file.length();
-
+	    _taille = file.length(); // Faux, rassurez moi ? c'est la taille des découpes
+	    file.close();
 	}
 	catch(IOException e){
 	    e.printStackTrace();
 	}
 	finally{
-	    f.close();
+	    //	    file.close();
 	}
+    }
+
+
+    public int getPort(){
+	return _port;
 
     }
     
+    public int getIp(){
+	return _ip;
+    }
+    
+    public int getNbConnexion(){
+	return _nb_connexion;
+    }
+    
+    public int getTmp(){
+	return _tmp_refresh;
+    }
+    
+    public long getTaille(){
+	return _taille;
+    }
+    
+    public void setNbConnexion(int nb){ // relève erreur si <0?
+	_nb_connexion = nb;
+    }
+    
+    public void setTaille(int taille){
+	_taille = taille;
+    }
+
+    public void saveValues(String fileName){
+	try{
+	    RandomAccessFile file = new RandomAccessFile(fileName,"rw");
+	    
+	    file.writeChars("IP : " + _ip + "\n");
+	    file.writeChars("Port : " + _port + "\n");
+	    file.writeChars("Nb Connexion : " + _nb_connexion + "\n");
+	    file.writeChars("Tmp Refresh : " + _tmp_refresh + "\n");
+	    file.writeChars("taille : " + _taille + "\n");
+	    
+	    file.close();
+	}
+	catch(IOException e){
+	    e.printStackTrace();
+	}
+	finally{
+	    //	    file.close();
+	}
+    }
+
+
     static private int recupererValeure(RandomAccessFile f,String champ) throws java.io.IOException {
 	f.seek(0);
 	String s = f.readLine();
@@ -57,11 +108,17 @@ class FichierConfiguration{
 	return -1;
     }
 
-    static public toString(){
-	System.out.println();
-
-
-	
+       
+    public String toString(){
+	return 
+	    "Option de configuration:\n" 
+	    + "Tracker : Ip: " + _ip +", port: " + _port + "\n"
+	    + "Nombre de connexion simultanées max : " + _nb_connexion + "\n"
+	    + "Taille de découpe des fichiers : " + _taille + "\n"
+	    + "Temps de refresh : " + _tmp_refresh + "\n";
     }
+
+    
+    
     
 }
--- paires/Test.java
+++ paires/Test.java
@@ -5,50 +5,23 @@
  *  Pour tester
  */
 
-import java.io.RandomAccessFile;
-import java.io.IOException;
-import java.lang.String;
 
+//package noeud;
 
 public class Test{
-
+    
     public static void main(String[] args){
 	System.out.println("Essais");
 	
-	try{
-	    
-	    String mot = new String("Truc : ");
-	    RandomAccessFile file = new RandomAccessFile("fichier.txt","r");
-	    
-	    System.out.println("Truc : " + recupererValeure(file,"Truc : "));
-	    System.out.println("Machin : " + recupererValeure(file,"Machin : "));
+	FichierConfiguration file = new FichierConfiguration("fichier.txt");
+	System.out.print(file.toString());
+	
+	file.setNbConnexion(file.getNbConnexion() + 2);
+	
+	file.saveValues("fichier.txt");
+	System.out.print(file.toString());
 
-	    file.close();
-	}
-	catch(IOException e){
-	    e.printStackTrace();
-	}
 	System.out.println("Fin test");
-    }
-
-
-    
-    static private int recupererValeure(RandomAccessFile f,String champ) throws java.io.IOException {
-	f.seek(0);
-	String s = f.readLine();
-	while(s != null){
-	    if (s.indexOf(champ)>=0){
-		int t = s.indexOf(champ)+champ.length();
-		String truc = String.copyValueOf(s.toCharArray(),t,s.length()-t);
-		int i = Integer.parseInt(truc);
-		return i;
-		//System.out.println(i);
-		
-	    }
-	    s = f.readLine();
-	}
-	// levée d'exception
-	return -1;
     }
     
 }
