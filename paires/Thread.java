c74918cfebd23a5677d92cf09ac53452d530f86e
blux <blux@blux-engine.(none)>
2012-03-16 13:33:46 +0100

    travail sur la gestion des Fichier

--- paires/Thread.java
+++
@@ -1,8 +0,0 @@
-public class Thread {
- 
-        public static void main(String[] args) {
-                
-                System.out.println("Le nom du thread principal est " + Thread.currentThread().getName());
-                
-        }
-}
