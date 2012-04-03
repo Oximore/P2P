a3a3308780d02e6f280b0ac9220eb472a8653e21
Oximore <blux@blux-engine.(none)>
2012-04-04 00:27:53 +0200

    Check-point

--- paires/ThreadUtilisateur.java
+++ paires/ThreadUtilisateur.java
@@ -39,11 +39,11 @@ public class ThreadUtilisateur extends Thread {
 	    OutputStream out = socket.getOutputStream(); // close-flush-write
 	    
 	    String str;
-	    int ret = 0, res;
+	    int ret = -1, res, ret2;
 	    byte[] b = new byte[1000];
-	    while (ret != 101){
+	    while (ret != 0){
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Recherche d'un nom de fichier(1) ou d'une clé(2) :");
+		System.out.println("Recherche d'un nom de fichier(1), d'une clé(2), quitter(0) :");
 		while (!scan.hasNextInt())
 		    str = scan.next();
 		ret = scan.nextInt();
@@ -64,8 +64,6 @@ public class ThreadUtilisateur extends Thread {
 		    out.flush();
 		}
 		
-		
-		
 		res = in.read(b);
 		String string = new String(b);
 		// Si end of stream
@@ -78,24 +76,24 @@ public class ThreadUtilisateur extends Thread {
 			Collecte co = new Collecte(string);
 			InfoPair tab[] = co.getTab();
 
-			System.out.println("Voici les fichiers disponibles, taper le numéro pour commencer le téléchargement :");
+			System.out.println("Voici les fichiers disponibles, taper le numéro pour commencer le téléchargement:");
 
-			for (int i=0; i<co.getTab().length ; i++){
+			for (int i=0; i<co.getTab().length ; i++)
 			    System.out.println(i + ":\t" + co.getTab()[i]);
-			    System.out.println(co.getTab().length + 1 +":\tAnnuler");
-			    
-			    while (!scan.hasNextInt())
-				str = scan.next();
-			    ret = scan.nextInt();
+			System.out.println(co.getTab().length + 1 +":\tAnnuler");
 			
-			    if (0 <= ret && ret <= co.getTab().length){
-				// lancer le téléchargement du fichier i
-				
-				
-			    } else if (ret == co.getTab().length+1){
-				// quitter
+			while (!scan.hasNextInt())
+			    str = scan.next();
+			ret2 = scan.nextInt();
+			
+			if (0 <= ret2 && ret2 <= co.getTab().length){
+			    // lancer le téléchargement du fichier i
 			    
-			    }
+			    //trouver les pairs dispo
+			    
+			} else if (ret2 == co.getTab().length+1){
+			    // quitter
+						    
 			}
 		    }		
 		}
@@ -111,7 +109,7 @@ public class ThreadUtilisateur extends Thread {
 	System.out.println("Bye bye !");
     }
     
-    
+    // *TODO* check ici si le fichier existe : @key -> @Fichier
     public void telecharger(String ip, int port, String key, int num[]){
 	// établire le lien avec l'autre pair
 	try {
@@ -126,26 +124,30 @@ public class ThreadUtilisateur extends Thread {
 	    
 	    String str;
 	    int ret = 0, res;
-	    byte[] b = new byte[1000];
+	    byte[] b = new byte[1000]; // *TODO* Pas assez grand je le crains ...
+	    String question = new String("getpieces " + key + " [");
+	    for (int i=0 ; i<num.length ; i++)
+		question += num[i] + " ";
+	    out.write((question.trim() + "]").getBytes());
+	    out.flush();
 	    
-	    for (int i=0 ; i<num.length ; i++){
-		
-		out.write(("getpieces " + key + " [").getBytes());
-		out.flush();
-		    
+	    while (ret != -1){
 		res = in.read(b);
 		String string = new String(b);
 		// Si end of stream
 		if (res == -1){
 		    throw new SocketException("connection au pair interrompue"); 
 		} else {
-		    if (-1 != string.indexOf("list")){
+		    if (-1 != string.indexOf("data")){
 			System.out.println("<< " + string);
-			
+			String key_tmp = string.split(" ")[1];
+			if (key != null)
+			    enregistre((Fichier)_hash.get(key_tmp), string.substring(1+string.indexOf("["),string.indexOf("]")).split(" "));
+			ret = -1;
 		    }		    
 		}
 	    }
-		
+	    
 	    out.close();
 	    in.close();
 	    socket.close();
@@ -155,6 +157,29 @@ public class ThreadUtilisateur extends Thread {
 	}
 	System.out.println("Déco d'un pair");
     }
-
-
+    
+    
+    public void enregistre(Fichier f, String [] infos){
+	System.out.println("fonction enregistre");
+	String [][] tab = new String[infos.length][2];
+	int indice;
+	boolean [] masque  = f.getMasque();;
+	for (int i=0 ; i<infos.length ; i++)
+	    tab[i] = infos[i].split(":"); 
+	
+	if (f != null){
+	    try{
+		RandomAccessFile file = new RandomAccessFile("Download/"+f.getName(), "rw");
+		for (int i=0 ; i<tab.length ; i++){
+		    indice = Integer.parseInt(tab[i][1]);
+		    file.seek(indice*f.getTaillePiece());
+		    file.writeBytes(tab[i][2]);
+		    masque[indice] = true;
+		}
+		file.close();
+	    } catch (IOException ioe) { System.out.println("ioe in enregistrer" + ioe); }
+	    
+	} else { System.out.println("premier para de enregistrer foireux"); }
+	f.setMasque(masque);
+    }
 }
